46bdade9425d3cbc3589f73e0cd6a5f3186326ef
Juan Menendez <juan@rydenusa.bike>
2024-10-01 16:17:40 -0700

    Initial Commit

---
+++ TestGitIntegration/TestGitIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitIntegrationApp.swift
+//  TestGitIntegration
+//
+//  Created by Juan Menendez on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGitIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
